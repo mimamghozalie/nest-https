1ba33c6c4732d9b0b8229275ba2a51cd9877da4f
Mochamad Imam Ghozalie <mimamghozalie@gmail.com>
2021-03-22 18:22:45 +0700

    add https optional

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,37 @@ import * as helmet from "helmet";
 import { readFileSync } from 'fs';
 import { Logger } from '@nestjs/common';
 
+const PORT = 3000;
+const SSL = true;
+
+let httpsOptions = null;
+if (SSL) {
+  httpsOptions = {
+    key: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/privkey.pem', 'utf8'),
+    cert: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/fullchain.pem', 'utf8'),
+    ca: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/chain.pem', 'utf8')
+  }
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      key: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/privkey.pem', 'utf8'),
-      cert: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/fullchain.pem', 'utf8'),
-      ca: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/chain.pem', 'utf8')
-    }
-  });
+  try {
+    const app = await NestFactory.create(AppModule, {
+      httpsOptions
+    });
 
 
-  app.use(helmet());
+    app.use(helmet());
 
-  await app.listen(3000);
+    await app.listen(3000);
 
-  Logger.log(`Server lister ${3000}`)
+    Logger.log(
+      `run at http${SSL ? 's' : ''}://localhost:${PORT}`,
+      'NEST Test Project',
+    );
+  } catch (error) {
+    Logger.error(error, error, 'Bootstrap');
+  }
 }
 bootstrap();
+
+
