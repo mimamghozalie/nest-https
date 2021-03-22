dc2ebb8597bdf79ee9c2e32dddf027548ae910c9
Mochamad Imam Ghozalie <mimamghozalie@gmail.com>
2021-03-22 18:06:47 +0700

    fix read ssl

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     httpsOptions: {
-      key: readFileSync('./etc/letsencrypt/live/yudha.aiva.store/privkey.pem', 'utf8'),
-      cert: readFileSync('./etc/letsencrypt/live/yudha.aiva.store/fullchain.pem', 'utf8'),
-      ca: readFileSync('./etc/letsencrypt/live/yudha.aiva.store/chain.pem', 'utf8')
+      key: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/privkey.pem', 'utf8'),
+      cert: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/fullchain.pem', 'utf8'),
+      ca: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/chain.pem', 'utf8')
     }
   });
 
