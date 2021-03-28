b431632c5a5bc490f50cb41f462949272a74c2bd
Mochamad Imam Ghozalie <mimamghozalie@gmail.com>
2021-03-28 17:48:55 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,19 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 import { readFileSync } from 'fs';
 
 // libs
-import { AppModule } from './app.module';
 import * as helmet from "helmet";
 
-const { SSL, PORT, NODE_ENV, APP_NAME } = process.env;
+import { AppModule } from './app.module';
+
+const { SSL, PORT, NODE_ENV, APP_NAME, DOMAIN } = process.env;
 const PRODUCTION = NODE_ENV == 'production' ? true : false;
 
 let httpsOptions = null;
 if (SSL == 'ON') {
   httpsOptions = {
-    key: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/privkey.pem', 'utf8'),
-    cert: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/fullchain.pem', 'utf8'),
-    ca: readFileSync('/etc/letsencrypt/live/yudha.aiva.store/chain.pem', 'utf8')
+    key: readFileSync(`/etc/letsencrypt/live/${DOMAIN}/privkey.pem`, 'utf8'),
+    cert: readFileSync(`/etc/letsencrypt/live/${DOMAIN}/fullchain.pem`, 'utf8'),
+    ca: readFileSync(`/etc/letsencrypt/live/${DOMAIN}/chain.pem`, 'utf8')
   }
 }
 
@@ -37,12 +38,14 @@ async function bootstrap() {
     if (PRODUCTION) {
       app.use(helmet())
       app.set('trust proxy', 1)
+    } else {
+      app.enableCors()
     }
 
     await app.listen(PORT);
 
     Logger.log(
-      `[${NODE_ENV.toUpperCase()}] run at http${SSL == 'ON' ? 's' : ''}://localhost:${PORT}`,
+      `[${NODE_ENV.toUpperCase()}] ${SSL == 'ON' ? '[SSL]' : ''}listen on port: ${PORT}`,
       `${APP_NAME}`, true,
     );
   } catch (error) {
